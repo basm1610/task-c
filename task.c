5816ff58b60a88ba443f6cbf45a9925d721dc8ec
basm1610 <86631963+basm1610@users.noreply.github.com>
2023-10-13 15:00:28 +0300

    finishing task

---
+++ task.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_USERS 5
+#define USERNAME_LENGTH 50
+#define PASSWORD_LENGTH 50
+
+typedef struct {
+    char username[USERNAME_LENGTH];
+    char password[PASSWORD_LENGTH];
+} 
+User;
+
+User users[MAX_USERS];
+int num_Users = 0;
+
+void registerUser() {
+    if (num_Users >= MAX_USERS) {
+        printf("Maximum number of users reached.\n");
+        // i used exit when use retun the loop is infinity but exit(0) print and close program. 
+        exit(0);
+    }
+
+    User newUser;
+    printf("Enter username: ");
+    scanf("%s", newUser.username);
+
+    // Check if the username already exists
+    for (int i = 0; i < num_Users; i++) {
+        if (strcmp(users[i].username, newUser.username) == 0) {
+            printf("Username already exists. Please choose a different username.\n");
+            return;
+        }
+    }
+
+    printf("Enter password: ");
+    scanf("%s", newUser.password);
+
+    users[num_Users] = newUser;
+    num_Users++;
+
+    printf("Registration successful.\n");
+}
+
+void loginUser() {
+    char username[USERNAME_LENGTH];
+    char password[PASSWORD_LENGTH];
+
+    printf("Enter username: ");
+    scanf("%s", username);
+
+    printf("Enter password: ");
+    scanf("%s", password);
+
+    for (int i = 0; i < num_Users; i++) {
+        if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0) {
+            printf("Login successful.\n");
+            return;
+        }
+    }
+
+    printf("Invalid username or password.\n");
+}
+
+int main() {
+    int choice;
+
+    while (1) {
+        printf("1. Register\n");
+        printf("2. Login\n");
+        printf("3. Exit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                registerUser();
+                break;
+            case 2:
+                loginUser();
+                break;
+            case 3:
+                printf("Exiting...\n");
+                exit(0);
+            default:
+                printf("Invalid choice. Please try again.\n");
+        }
+
+        printf("\n");
+    }
+}
