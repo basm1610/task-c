7789a52a11b41dd977b22a62730375e808fe8916
basm1610 <86631963+basm1610@users.noreply.github.com>
2023-11-13 21:09:16 +0200

    finishing task-2

--- task.c
+++ task.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define MAX_USERS 5
 #define USERNAME_LENGTH 50
 #define PASSWORD_LENGTH 50
 
-typedef struct {
+typedef struct
+{
     char username[USERNAME_LENGTH];
     char password[PASSWORD_LENGTH];
-} 
-User;
+} User;
+union type
+{
+    bool isActive;
 
+} activation;
 User users[MAX_USERS];
 int num_Users = 0;
 
-void registerUser() {
-    if (num_Users >= MAX_USERS) {
+void registerUser()
+{
+    if (num_Users >= MAX_USERS)
+    {
         printf("Maximum number of users reached.\n");
-        // i used exit when use retun the loop is infinity but exit(0) print and close program. 
+        // i used exit when use retun the loop is infinity but exit(0) print and close program.
+
         exit(0);
     }
 
@@ -27,8 +35,10 @@ void registerUser() {
     scanf("%s", newUser.username);
 
     // Check if the username already exists
-    for (int i = 0; i < num_Users; i++) {
-        if (strcmp(users[i].username, newUser.username) == 0) {
+    for (int i = 0; i < num_Users; i++)
+    {
+        if (strcmp(users[i].username, newUser.username) == 0)
+        {
             printf("Username already exists. Please choose a different username.\n");
             return;
         }
@@ -39,11 +49,14 @@ void registerUser() {
 
     users[num_Users] = newUser;
     num_Users++;
+    printf("Have you Active true[1],false[0]: ");
+    scanf("%d", &activation.isActive);
 
     printf("Registration successful.\n");
 }
 
-void loginUser() {
+void loginUser()
+{
     char username[USERNAME_LENGTH];
     char password[PASSWORD_LENGTH];
 
@@ -53,38 +66,43 @@ void loginUser() {
     printf("Enter password: ");
     scanf("%s", password);
 
-    for (int i = 0; i < num_Users; i++) {
-        if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0) {
+    for (int i = 0; i < num_Users; i++)
+    {
+        if (activation.isActive && strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0)
+        {
             printf("Login successful.\n");
             return;
         }
     }
 
-    printf("Invalid username or password.\n");
+    printf("Invalid username or password or Activetion is false[0].\n");
 }
 
-int main() {
+int main()
+{
     int choice;
 
-    while (1) {
+    while (1)
+    {
         printf("1. Register\n");
         printf("2. Login\n");
         printf("3. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
-        switch (choice) {
-            case 1:
-                registerUser();
-                break;
-            case 2:
-                loginUser();
-                break;
-            case 3:
-                printf("Exiting...\n");
-                exit(0);
-            default:
-                printf("Invalid choice. Please try again.\n");
+        switch (choice)
+        {
+        case 1:
+            registerUser();
+            break;
+        case 2:
+            loginUser();
+            break;
+        case 3:
+            printf("Exiting...\n");
+            exit(0);
+        default:
+            printf("Invalid choice. Please try again.\n");
         }
 
         printf("\n");
